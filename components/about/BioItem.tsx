9065af2cfbeba6c6ca558060f9b46797afcb9b3e
Alex Escobar <202838954+alex-escobar-h@users.noreply.github.com>
2025-05-29 00:18:29 -0700

    basic gsap animation for BioItem

--- components/about/BioItem.tsx
+++ components/about/BioItem.tsx
@@ -1,21 +1,76 @@
+"use client";
 import type { Bio } from "@/utils/types";
 import { formatID } from "@/utils/utilFunctions";
 import { Accent } from "../Accent";
 
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+import { useRef } from "react";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger, SplitText);
+
 export default function BioItem({ id, title, content }: Bio) {
+  const bioRef = useRef(null);
+
+  useGSAP(
+    () => {
+      if (!bioRef.current) return;
+      const content = SplitText.create(".bio-content", {
+        type: "lines",
+        mask: "lines",
+      });
+      console.log(content);
+      const tl = gsap.timeline({
+        scrollTrigger: { trigger: bioRef.current, start: "top 90%" },
+      });
+      tl.fromTo(
+        ".bio-divider",
+        {
+          scaleX: 0,
+        },
+        {
+          scaleX: 1,
+          transformOrigin: "left",
+        }
+      )
+        .fromTo(
+          content.lines,
+          {
+            opacity: 0,
+            y: 20,
+          },
+          { opacity: 0.8, y: 0, stagger: 0.05 }
+        )
+        .fromTo(
+          ".bio-header",
+          {
+            opacity: 0,
+            y: 20,
+          },
+          { opacity: 0.8, y: 0 },
+          "<"
+        );
+    },
+    { scope: bioRef }
+  );
   return (
-    <article className='flex flex-col gap-4 md:grid md:grid-cols-12'>
+    <article
+      ref={bioRef}
+      className='flex flex-col gap-4 md:grid md:grid-cols-12'
+    >
       {/* HR Start ----------------------- */}
-      <hr className='md:col-span-full opacity-10 ' />
+      <hr className='bio-divider md:col-span-full opacity-30 ' />
       {/* HR End ------------------------- */}
 
       {/* Header Start ------------------- */}
-      <header className='flex gap-4 md:gap-0 md:grid md:grid-cols-subgrid md:row-start-2 md:col-start-1 md:col-span-3'>
-        <Accent className='text-start font-mono font-light md:col-start-1 md:col-span-1 '>
+      <header className='bio-header flex gap-4 md:gap-0 md:grid md:grid-cols-subgrid md:row-start-2 md:col-start-1 md:col-span-3 overflow-hidden'>
+        <Accent className='bio-id text-start font-mono font-light md:col-start-1 md:col-span-1 '>
           ({formatID(id)})
         </Accent>
 
-        <h3 className='md:grid md:col-start-2 md:col-span-4 md:row-start-1'>
+        <h3 className='bio-title md:grid md:col-start-2 md:col-span-4 md:row-start-1'>
           {title}
         </h3>
       </header>
@@ -23,7 +78,7 @@ export default function BioItem({ id, title, content }: Bio) {
 
       {/* Content Start ------------------ */}
       <div className='md:col-start-7 md:col-span-6 lg:col-start-9 lg:col-span-4'>
-        <p className='text-sm md:max-w-[45ch] text-base-content-100/60'>
+        <p className='bio-content text-sm md:max-w-[45ch] text-base-content-100/60'>
           {content}
         </p>
       </div>
