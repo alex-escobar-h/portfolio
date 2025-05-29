9b2b499442344776176019a39a10b589dafd425e
Alex Escobar <202838954+alex-escobar-h@users.noreply.github.com>
2025-05-28 23:04:16 -0700

    Menu button for mobile devices

---
+++ components/buttons/MenuButton.tsx
@@ -0,0 +1,75 @@
+"use client";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useEffect, useRef, useState } from "react";
+import MobileMenu from "../menu/MobileMenu";
+
+gsap.registerPlugin(useGSAP);
+
+export default function MenuButton() {
+  const menuBtnRef = useRef(null);
+  const [clicked, setClicked] = useState(false);
+  const tlRef = useRef<GSAPTimeline>(null);
+
+  const toggleMenuButton = () => {
+    setClicked((prev) => !prev);
+  };
+
+  useGSAP(
+    () => {
+      const tl = gsap.timeline({ paused: true });
+      tl.to(".line-1", {
+        y: 4,
+        duration: 0.2,
+      })
+        .to(
+          ".line-2",
+          {
+            y: -4,
+            duration: 0.2,
+          },
+          "<"
+        )
+        .to(".line-1", {
+          rotate: 45,
+          transformOrigin: "center",
+          duration: 0.2,
+          delay: 0.05,
+        })
+        .to(
+          ".line-2",
+          { rotate: -45, transformOrigin: "center", duration: 0.2 },
+          "<"
+        );
+
+      tlRef.current = tl;
+    },
+    { scope: menuBtnRef }
+  );
+
+  useEffect(() => {
+    const tl = tlRef.current;
+    if (!tl) return;
+    clicked ? tl.play() : tl.reverse();
+  }, [clicked]);
+
+  return (
+    <>
+      <button
+        ref={menuBtnRef}
+        className='w-6 h-2 relative cursor-pointer z-10 md:hidden'
+        onClick={toggleMenuButton}
+      >
+        <div className='line line-1 w-full h-px bg-base-content-100 absolute top-0' />
+        <div className='line line-2 w-full h-px bg-base-content-100 absolute top-full' />
+      </button>
+      {clicked && (
+        <MobileMenu
+          handleNavClick={() => {
+            setClicked(false);
+          }}
+        />
+      )}
+    </>
+  );
+}
