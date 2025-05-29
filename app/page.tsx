c24962e4b53053e38c2e6b7dbf8fea2f69ea14b9
Alex Escobar <202838954+alex-escobar-h@users.noreply.github.com>
2025-05-29 15:42:02 -0700

    styles: create sticky layouts

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import About from "@/components/about/About";
 import Footer from "@/components/footer/Footer";
 import Hero from "@/components/Hero";
@@ -8,12 +9,24 @@ import Project from "@/components/project/Project";
 export default function Home() {
   return (
     <>
-      {/* <HueInitializer /> */}
       <Navbar />
-      <Hero />
-      <About />
-      <Project />
-      <Footer />
+      <div className='sticky top-0 left-0'>
+        <Hero />
+      </div>
+      <div className='relative'>
+        <main
+          id='main'
+          className='relative z-20 bg-base-100 pb-48'
+        >
+          <About />
+          <Project />
+        </main>
+        <Footer />
+        <span
+          id='contact'
+          className='scroll-mb-[100000000vh]'
+        />
+      </div>
     </>
   );
 }
--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -24,15 +24,15 @@ export default function Footer() {
 
       gsap.fromTo(
         title.chars,
-        { y: 250 },
+        { y: 500 },
         {
           y: 0,
-          ease: "back.out",
+          ease: "sine",
           stagger: 0.1,
           duration: 1,
           scrollTrigger: {
-            trigger: footerRef.current,
-            start: "90% bottom",
+            trigger: document.querySelector("#main"),
+            start: "bottom center",
           },
         }
       );
@@ -43,61 +43,72 @@ export default function Footer() {
   return (
     <footer
       ref={footerRef}
-      id='contact'
-      className='pt-section'
+      className='pt-section border-b-8 sm:border-b-[0.75rem] md:border-b-[1.5rem] lg:border-b-[2rem] bg-base-100 h-screen flex flex-col justify-end sticky bottom-0 left-0'
     >
-      {/* Email ================ */}
-      <hr />
-      <div className='container mx-auto px-2 pt-16'>
-        <div className='mb-4'>
-          <h3 className=''>Feel free to reach out to me</h3>
-          <a
-            href={`mailto:${email.href}`}
-            aria-label={`Send an email to ${email.href}`}
-            className='flex items-center gap-2 text-xl w-fit text-base-content-100/70'
-          >
-            <span className='text-4xl font-extralight'>↳</span>
-            <span className='underline underline-offset-2 font-light'>
-              {email.href}
-            </span>
-          </a>
+      <div>
+        <div className='space-y-4'>
+          <hr />
+          <hr />
+          <hr />
+          <hr />
         </div>
-        {/* Email ================ */}
-
-        {/* Links ================ */}
         <div>
-          <h3>Find me at</h3>
-          <ul>
-            {externalLinks.map((link) => (
-              <li key={link.id}>
-                <a
-                  href={link.href}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='font-light text-base-content-100/75'
-                >
-                  <span className='underline underline-offset-2'>
-                    {link.title}
-                  </span>
-                  <span className='no-underline inline-block px-0.5'>↗</span>
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-        {/* Links ================ */}
-      </div>
+          {/* Email ================ */}
 
-      {/* Titles ================ */}
-      <div className='text-center font-semibold overflow-hidden'>
-        <h2
-          className='contact-title overflow-hidden'
-          style={{ fontSize: "clamp(4rem, -0.9412rem + 22.5882vw, 16rem)" }}
-        >
-          Contact
-        </h2>
+          <div className='container mx-auto px-2 pt-16'>
+            <div className='mb-4'>
+              <h3 className=''>Feel free to reach out to me</h3>
+              <a
+                href={`mailto:${email.href}`}
+                aria-label={`Send an email to ${email.href}`}
+                className='flex items-center gap-2 text-lg w-fit text-base-content-100/70'
+              >
+                <span className='text-4xl font-extralight'>↳</span>
+                <span className='underline underline-offset-2 font-light'>
+                  {email.href}
+                </span>
+              </a>
+            </div>
+            {/* Email ================ */}
+
+            {/* Links ================ */}
+            <div>
+              <h3>Find me at</h3>
+              <ul>
+                {externalLinks.map((link) => (
+                  <li key={link.id}>
+                    <a
+                      href={link.href}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      className='font-light text-base-content-100/75'
+                    >
+                      <span className='underline underline-offset-2'>
+                        {link.title}
+                      </span>
+                      <span className='no-underline inline-block px-1'>↗</span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {/* Links ================ */}
+          </div>
+        </div>
+        {/* Titles ================ */}
+        <div className='text-center font-semibold overflow-hidden'>
+          <h2
+            className='contact-title overflow-hidden'
+            style={{
+              fontSize: "clamp(4rem, -0.9412rem + 22.5882vw, 16rem)",
+              lineHeight: 1.125,
+            }}
+          >
+            Contact
+          </h2>
+        </div>
+        {/* Titles end ================ */}
       </div>
-      {/* Titles ================ */}
     </footer>
   );
 }
