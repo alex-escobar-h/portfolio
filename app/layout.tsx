86c38f28d7a15853f6f7da6a630f624feec4e353
Alex Escobar <202838954+alex-escobar-h@users.noreply.github.com>
2025-05-29 17:02:07 -0700

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,18 @@ import { inter } from "@/utils/fonts";
 export const metadata: Metadata = {
   title: "Portfolio | Alex Escobar",
   description: "Portfolio by Alexander Escobar",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+    shortcut: "/favicon-16x16.png",
+    other: [
+      {
+        rel: "icon",
+        url: "/favicon-32x32.png",
+        sizes: "32x32",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
